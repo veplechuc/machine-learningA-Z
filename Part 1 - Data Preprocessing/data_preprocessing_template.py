33933ad8da0b009d4606b1d6d90872b440c8c8cf
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-03-26 11:14:46 -0300

    refactoring code.

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
@@ -11,7 +12,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
